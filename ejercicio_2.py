7e46a4a1fe61aa4fd294d4475050b9d4e496cfe1
Garcia Fabian Emmanuel <85421529+garcia-f@users.noreply.github.com>
2024-03-22 09:45:25 -0300

    correccion del ejercicio 2

--- ejercicio_2.py
+++ ejercicio_2.py
@@ -1,20 +1,28 @@
 
-# dada una lista de numeros, quitar los repetidos.
-listas = [ 1, 2, 3, 3, 2, 2, 4, 6, 7, 7, 7, 1 ]
+# Función que elimina los valores repetidos de una lista y retorna una nueva lista sin los valores repetidos.
+def quitar_repetidos(lista):
+    lista_sin_repetidos = list(set(lista))
+    return lista_sin_repetidos
 
-def clean_list( num ):
-    lista = set(num)          # un set no puede tener datos repetidos
-    return list( lista )
+# Función que ordena los valores distintos de una lista y cuenta cuántos hay de cada uno.
+# Retorna un diccionario donde las claves son los valores distintos y los valores son las cantidades de cada uno.
+def contar_y_ordenar(lista):
+    conteo = {}
+    for elemento in lista:
+        if elemento in conteo:
+            conteo[elemento] += 1
+        else:
+            conteo[elemento] = 1
+    
+    # Ordenar el diccionario por los valores (cantidad de ocurrencias) en orden ascendente.
+    conteo_ordenado = {k: v for k, v in sorted(conteo.items(), key=lambda item: item[1])}
+    
+    return conteo_ordenado
 
-print( clean_list( listas ) )
+# Ejemplo de uso:
+lista_ejemplo = [1, 2, 3, 3, 2, 2, 4, 6, 7, 7, 7, 1]
+lista_sin_repetidos = quitar_repetidos(lista_ejemplo)
+conteo_ordenado = contar_y_ordenar(lista_ejemplo)
 
-
-# dada una lista de numeros, cuantos repetidos hay.
-
-def repetidos( num ):
-    if len( num ) == len( set( num ) ):
-        print('No hay repetidos')
-    else:
-        print('Hay repetidos')
-
-repetidos( listas ) 
+print("Lista sin valores repetidos:", lista_sin_repetidos)
+print("Valores distintos ordenados y su cantidad de ocurrencias:", conteo_ordenado)
